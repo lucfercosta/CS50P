d992c90d9493511662b4809ee8a01f944715a174
Lucas <72714237+lucfercosta@users.noreply.github.com>
2025-11-04 16:51:49 +0000

    Add files via upload

---
+++ 03 - Exceptions/01 - taqueria/taqueria.py
@@ -0,0 +1,23 @@
+total = 0
+menu = {
+    "Baja Taco": 4.25,
+    "Burrito": 7.50,
+    "Bowl": 8.50,
+    "Nachos": 11.00,
+    "Quesadilla": 8.50,
+    "Super Burrito": 8.50,
+    "Super Quesadilla": 9.50,
+    "Taco": 3.00,
+    "Tortilla Salad": 8.00
+}
+
+while True:
+    try:
+        order = input("Item: ").title()
+    except EOFError:
+        break
+    
+    if order in menu:
+        total += menu[order]
+        
+print(f"\nTotal: {total:.2f}")    
